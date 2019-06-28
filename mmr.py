529c9b393dfd6cefd55057c0e51fce090a5b6fb6
jjy <jjyruby@gmail.com>
2019-06-28 17:27:05 +0800

    use bagging peaks to compute root

--- mmr.py
+++ mmr.py
@@ -32,6 +32,32 @@ def sibling_offset(height) -> int:
     return 2 ** (height + 1) - 1
 
 
+def get_peaks(mmr_size) -> [int]:
+    """
+    return peaks from left to right
+    """
+    poss = []
+    height, pos = left_peak_height_pos(mmr_size)
+    poss.append(pos)
+    while height > 0:
+        height, pos = get_right_peak(height, pos, mmr_size)
+        poss.append(pos)
+    return poss
+
+
+def get_right_peak(height, pos, mmr_size):
+    """
+    find next right peak
+    """
+    # jump to right sibling
+    pos += sibling_offset(height)
+    # jump to left child
+    while pos > mmr_size - 1:
+        pos -= 2 ** height
+        height -= 1
+    return (height, pos)
+
+
 # TODO optimize this
 def left_peak_height_pos(mmr_size: int) -> (int, int):
     height = 0
@@ -78,69 +104,75 @@ class MMR(object):
             self.pos_hash[self.last_pos] = hasher.digest()
             height += 1
 
-    def get_peaks(self) -> [int]:
-        """
-        return peaks from left to right
-        """
-        poss = []
-        height, pos = left_peak_height_pos(self.last_pos + 1)
-        poss.append(pos)
-        while height > 0:
-            height, pos = self._get_right_peak(height, pos)
-            poss.append(pos)
-        return poss
-
-    def _get_right_peak(self, height, pos):
-        """
-        find next right peak
-        """
-        # jump to right sibling
-        pos += sibling_offset(height)
-        # jump to left child
-        while pos > self.last_pos:
-            pos -= 2 ** height
-            height -= 1
-        return (height, pos)
-
     def get_root(self) -> bin:
-        height, pos = left_peak_height_pos(self.last_pos + 1)
-        if pos == self.last_pos:
-            return self.get_hash(pos, height)
+        peaks = get_peaks(self.last_pos + 1)
+        root = self._bag_rhs_peaks(0, peaks)
+        if root is not None:
+            return root[1]
         else:
-            pos += 2 ** (height + 1)
-            return self.get_hash(pos, height + 1)
+            return None
 
-    # convert leaf pos to inner pos
-    # TODO optimize this
     def gen_proof(self, pos: int) -> [bin]:
         """
         generate a merkle proof
-        1. find and push sibling hash
-           1. calculate virtual sibling if reach a peak
-        2. increase height
-        3. return proof if reach MMR height(height + 1)
+        1. generate merkle tree proof for pos
+        2. find peaks positions
+        3. find rhs peaks packs into one single hash, then push to proof
+        4. find lhs peaks reverse then push to proof
+
+        For peaks: P1, P2, P3, P4, P5, we proof a P4 leaf
+        [P4 merkle proof.., P5, P3, P2, P1]
         """
         height, _pos = left_peak_height_pos(self.last_pos + 1)
         proof = []
-        proof_size = height + 1
         height = 0
-        while len(proof) < proof_size:
+        # while len(proof) < proof_size:
+        while pos <= self.last_pos:
             pos_height = tree_height(pos)
             next_height = tree_height(pos + 1)
             if next_height > pos_height:
                 # get left child sib
                 sib = pos - sibling_offset(height)
+                if sib not in self.pos_hash:
+                    break
                 proof.append((sib, self.get_hash(sib, height)))
                 # goto parent node
                 pos += 1
             else:
                 # get right child
                 sib = pos + sibling_offset(height)
+                if sib not in self.pos_hash:
+                    break
                 proof.append((sib, self.get_hash(sib, height)))
                 # goto parent node
                 pos += 2 ** (height + 1)
             height += 1
+        peak_pos = pos
+        peaks = get_peaks(self.last_pos + 1)
+        # bagging rhs peaks into one hash
+        rhs_peak_hash = self._bag_rhs_peaks(peak_pos, peaks)
+        if rhs_peak_hash is not None:
+            proof.append(rhs_peak_hash)
+        proof.extend(reversed(self._lhs_peaks(peak_pos, peaks)))
         return MerkleProof(mmr_size=self.last_pos + 1, proof=proof)
+
+    def _bag_rhs_peaks(self, peak_pos: int, peaks: [int]):
+        rhs_peak_hashes = [(p, self.pos_hash[p]) for p in peaks
+                           if p > peak_pos]
+        while len(rhs_peak_hashes) > 1:
+            peak_r = rhs_peak_hashes.pop()
+            peak_l = rhs_peak_hashes.pop()
+            hasher = self._hasher()
+            hasher.update(peak_r[1])
+            hasher.update(peak_l[1])
+            rhs_peak_hashes.append((peak_r[0] + peak_l[0], hasher.digest()))
+        if len(rhs_peak_hashes) > 0:
+            return rhs_peak_hashes[0]
+        else:
+            return None
+
+    def _lhs_peaks(self, peak_pos: int, peaks: [int]):
+        return [(p, self.pos_hash[p]) for p in peaks if p < peak_pos]
 
     def get_hash(self, pos: int, height: int) -> bin:
         """
@@ -179,24 +211,30 @@ class MerkleProof(object):
         return hashlib.sha256()
 
     def verify(self, root: bin, pos: int, elem: bin) -> bool:
-        height, _pos = left_peak_height_pos(self.mmr_size)
-        # proof is log(n), exactly is our height + 1
-        if len(self.proof) != height + 1:
-            raise ValueError("invalid proof size")
+        peaks = get_peaks(self.mmr_size)
         hasher = self._hasher()
         hasher.update(elem)
         elem_hash = hasher.digest()
         height = 0
         logging.debug('proof is %s', self.proof)
-        logging.debug('9 is %s', self.mmr.pos_hash[9])
-        logging.debug('13 is %s', self.mmr.pos_hash[13])
-        logging.debug('14 is %s', self.mmr.pos_hash[14])
-        logging.debug('30 is %s', self.mmr.get_hash(30, 4))
         for (proof_pos, proof) in self.proof:
             hasher = self._hasher()
-            pos_heigh = tree_height(pos)
-            next_heigh = tree_height(pos + 1)
-            if next_heigh > pos_heigh:
+            # verify bagging peaks
+            if pos in peaks:
+                if pos == peaks[-1]:
+                    hasher.update(elem_hash)
+                    hasher.update(proof)
+                else:
+                    hasher.update(proof)
+                    hasher.update(elem_hash)
+                    pos = peaks[-1]
+                elem_hash = hasher.digest()
+                continue
+
+            # verify merkle path
+            pos_height = tree_height(pos)
+            next_height = tree_height(pos + 1)
+            if next_height > pos_height:
                 logging.debug('right %s', pos)
                 logging.debug('proof %s', proof)
                 # we are in right child
@@ -216,6 +254,9 @@ class MerkleProof(object):
             elem_hash = hasher.digest()
             height += 1
             logging.debug('----parent is %s %s %s', proof_pos, elem_hash, pos)
+        logging.debug('----parent is %s %s %s', proof_pos, elem_hash, pos)
+        logging.debug(elem_hash)
+        logging.debug(root)
         return elem_hash == root
 
 
