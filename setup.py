09cc3770e0e0a3063af42e4140bfcdfd139ad20b
jjy <jjyruby@gmail.com>
2019-06-30 10:15:20 +0800

    bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mmr.py",
-    version="0.0.2",
+    version="0.0.3",
     author="jjy",
     author_email="jjyruby@gmail.com",
     test_suite="tests",
